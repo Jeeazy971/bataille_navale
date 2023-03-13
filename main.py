b931096c9a52cc48c414e6176f70e0713c7e7514
Josué BAYIDIKILA <josue.dev@outlook.fr>
2023-03-13 15:12:46 +0100

    Partie 1 placement des grilles faites

--- main.py
+++ main.py
@@ -1,111 +1,107 @@
+# Import de la librairie pour générer des nombres aléatoires
 import random
 
-# Constantes
+# Définition de la taille de la grille de jeu
 TAILLE_GRILLE = 10
-BATEAUX = {"Porte-avions": 5, "Croiseur": 4, "Destroyer": 3, "Sous-marin": 3, "Torpilleur": 2}
-SYMBOLS = {"vide": "·", "bateau": "■", "touche": "X", "coule": "O"}
+
+# Définition de la liste des lettres utilisées pour identifier les colonnes
+LETTRES = "ABCDEFGHIJ"
+
+# Définition des constantes pour les différentes parties des navires
+TOUCHE = False
+INTACTE = True
+
+# Initialisation de la liste des navires
+liste_navires = ["porte_avion", "croiseur", "contre_torpilleur", "sous_marin", "torpilleur"]
+
+# Initialisation des dictionnaires pour chaque navire
+porte_avion = {}
+croiseur = {}
+contre_torpilleur = {}
+sous_marin = {}
+torpilleur = {}
 
 
-# Fonctions
-def initialiser_grille():
-    grille = []
-    for i in range(TAILLE_GRILLE):
-        ligne = []
-        for j in range(TAILLE_GRILLE):
-            ligne.append(SYMBOLS["vide"])
-        grille.append(ligne)
-    return grille
-
-
-def afficher_grille(grille):
-    print("  ", end="")
-    for i in range(TAILLE_GRILLE):
-        print(i, end=" ")
-    print()
-    for i in range(TAILLE_GRILLE):
-        print(i, end=" ")
-        for j in range(TAILLE_GRILLE):
-            print(grille[i][j], end=" ")
-        print()
-
-
-
-def placer_bateaux(grille):
-    for nom, taille in BATEAUX.items():
-        bateau_place = False
-        while not bateau_place:
-            x = random.randint(0, TAILLE_GRILLE - 1)
-            y = random.randint(0, TAILLE_GRILLE - 1)
-            direction = random.choice(["horizontal", "vertical"])
-            if direction == "horizontal":
-                if y + taille > TAILLE_GRILLE:
-                    continue
-                bateau = grille[x][y:y+taille]
-                if SYMBOLS["bateau"] in bateau:
-                    continue
-                for i in range(taille):
-                    grille[x][y+i] = SYMBOLS["bateau"]
+# Définition d'une fonction pour demander les coordonnées au joueur
+def demander_coordonnees():
+    while True:
+        # Demande des coordonnées au joueur
+        coordonnees = input("Entrez les coordonnées (lettre,chiffre) ex: (A5) : ")
+        # Vérification des coordonnées saisies
+        if len(coordonnees) != 2 or coordonnees[0].upper() not in LETTRES or not coordonnees[1].isdigit():
+            print("Coordonnées invalides, veuillez réessayer.")
+        else:
+            # Conversion des coordonnées en entiers
+            ligne = int(coordonnees[1])
+            colonne = LETTRES.index(coordonnees[0].upper()) + 1
+            # Vérification que les coordonnées sont dans la grille
+            if ligne < 1 or ligne > TAILLE_GRILLE or colonne < 1 or colonne > TAILLE_GRILLE:
+                print("Coordonnées en dehors de la grille, veuillez réessayer.")
             else:
-                if x + taille > TAILLE_GRILLE:
-                    continue
-                bateau = [grille[i][y] for i in range(x, x + taille)]
-                if SYMBOLS["bateau"] in bateau:
-                    continue
-                for i in range(taille):
-                    grille[x+i][y] = SYMBOLS["bateau"]
-            bateau_place = True
+                return (ligne, colonne)
 
 
-def tirer(grille, x, y):
-    if grille[x][y] == SYMBOLS["vide"]:
-        grille[x][y] = SYMBOLS["touche"]
-        resultat = "raté"
-    elif grille[x][y] == SYMBOLS["bateau"]:
-        grille[x][y] = SYMBOLS["coule"]
-        resultat = "touché"
-    else:
-        resultat = "déjà touché"
-    return resultat
+# Définition d'une fonction pour placer un navire sur la grille
+def placer_navire(navire, taille_navire):
+    while True:
+        # Demande des coordonnées de la première case
+        print(f"Placement du navire de taille {taille_navire}")
+        print(f"Entrez les coordonnées de la première case (lettre,chiffre) :")
+        coordonnees = demander_coordonnees()
+        ligne, colonne = coordonnees
+        # Vérification que le navire rentre dans la grille
+        if colonne + taille_navire > TAILLE_GRILLE + 1:
+            print("Le navire dépasse de la grille, veuillez réessayer.")
+            continue
+        # Placement du navire
+        for i in range(taille_navire):
+            case = (ligne, colonne + i)
+            navire[case] = INTACTE
+        return
 
 
-# Code principal
-print("Bienvenue dans le jeu de bataille navale !")
-grille_joueur = initialiser_grille()
-grille_ordinateur = initialiser_grille()
-placer_bateaux(grille_joueur)
-placer_bateaux(grille_ordinateur)
-afficher_grille(grille_joueur)
+# Placement des navires sur la grille
+tailles_navires = [5, 4, 3, 3, 2]
+for i, navire in enumerate(liste_navires):
+    placer_navire(eval(navire), tailles_navires[i])
+    print(f"Navire de taille {tailles_navires[i]} placé !")
 
-while True:
-    # Tour du joueur
-    print("C'est à votre tour de jouer.")
-    x_joueur = int(input("Entrez la coordonnée x : "))
-    y_joueur = int(input("Entrez la coordonnée y : "))
-    resultat_joueur = tirer(grille_ordinateur, x_joueur, y_joueur)
-    afficher_grille(grille_ordinateur)
-    if all(SYMBOLS["bateau"] not in ligne for ligne in grille_ordinateur):
-        print("Félicitations, vous avez gagné !")
-        break
-    if resultat_joueur == "raté":
-        print("Vous avez raté votre tir.")
-    elif resultat_joueur == "touché":
-        print("Vous avez touché un bateau !")
-    else:
-        print("Vous avez déjà touché cette case.")
+# Affichage de la grille
+grille = []
+for i in range(TAILLE_GRILLE):
+    ligne = []
+    for j in range(TAILLE_GRILLE):
+        case = (i + 1, j + 1)
+        if case in porte_avion:
+            if porte_avion[case] == TOUCHE:
+                ligne.append("X")
+            else:
+                ligne.append("P")
+        elif case in croiseur:
+            if croiseur[case] == TOUCHE:
+                ligne.append("X")
+            else:
+                ligne.append("C")
+        elif case in contre_torpilleur:
+            if contre_torpilleur[case] == TOUCHE:
+                ligne.append("X")
+            else:
+                ligne.append("T")
+        elif case in sous_marin:
+            if sous_marin[case] == TOUCHE:
+                ligne.append("X")
+            else:
+                ligne.append("S")
+        elif case in torpilleur:
+            if torpilleur[case] == TOUCHE:
+                ligne.append("X")
+            else:
+                ligne.append("T")
+        else:
+            ligne.append(".")
+    grille.append(ligne)
 
-    # Tour de l'ordinateur
-    print("C'est au tour de l'ordinateur.")
-    x_ordinateur = random.randint(0, TAILLE_GRILLE - 1)
-    y_ordinateur = random.randint(0, TAILLE_GRILLE - 1)
-    resultat_ordinateur = tirer(grille_joueur, x_ordinateur, y_ordinateur)
-    print("L'ordinateur tire en ({}, {}).".format(x_ordinateur, y_ordinateur))
-    if all(SYMBOLS["bateau"] not in ligne for ligne in grille_joueur):
-        print("L'ordinateur a gagné !")
-        break
-    if resultat_ordinateur == "raté":
-        print("L'ordinateur a raté son tir.")
-    elif resultat_ordinateur == "touché":
-        print("L'ordinateur a touché l'un de vos bateaux !")
-    else:
-        print("L'ordinateur a déjà touché cette case.")
-    afficher_grille(grille_joueur)
+# Affichage de la grille
+print("  " + " ".join(LETTRES))
+for i, ligne in enumerate(grille):
+    print(f"{i + 1:2} {' '.join(ligne)}")
