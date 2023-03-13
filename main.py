991ed12fb1e31174fa981199461c31192a025d03
Josué BAYIDIKILA <josue.dev@outlook.fr>
2023-03-13 14:30:42 +0100

    Finalisation du jeu de bataille navale (corrigé)

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def afficher_grille(grille):
         print()
 
 
+
 def placer_bateaux(grille):
     for nom, taille in BATEAUX.items():
         bateau_place = False
@@ -37,21 +38,21 @@ def placer_bateaux(grille):
             y = random.randint(0, TAILLE_GRILLE - 1)
             direction = random.choice(["horizontal", "vertical"])
             if direction == "horizontal":
-                if x + taille > TAILLE_GRILLE:
+                if y + taille > TAILLE_GRILLE:
                     continue
-                bateau = grille[x:x + taille][y]
+                bateau = grille[x][y:y+taille]
                 if SYMBOLS["bateau"] in bateau:
                     continue
                 for i in range(taille):
-                    grille[x + i][y] = SYMBOLS["bateau"]
+                    grille[x][y+i] = SYMBOLS["bateau"]
             else:
-                if y + taille > TAILLE_GRILLE:
+                if x + taille > TAILLE_GRILLE:
                     continue
                 bateau = [grille[i][y] for i in range(x, x + taille)]
                 if SYMBOLS["bateau"] in bateau:
                     continue
                 for i in range(taille):
-                    grille[x][y + i] = SYMBOLS["bateau"]
+                    grille[x+i][y] = SYMBOLS["bateau"]
             bateau_place = True
 
 
@@ -78,32 +79,33 @@ afficher_grille(grille_joueur)
 while True:
     # Tour du joueur
     print("C'est à votre tour de jouer.")
-    x = int(input("Entrez la coordonnée x : "))
-    y = int(input("Entrez la coordonnée y : "))
-    resultat = tirer(grille_ordinateur, x, y)
+    x_joueur = int(input("Entrez la coordonnée x : "))
+    y_joueur = int(input("Entrez la coordonnée y : "))
+    resultat_joueur = tirer(grille_ordinateur, x_joueur, y_joueur)
     afficher_grille(grille_ordinateur)
     if all(SYMBOLS["bateau"] not in ligne for ligne in grille_ordinateur):
         print("Félicitations, vous avez gagné !")
         break
-        if resultat == "raté":
-            print("Vous avez raté votre tir.")
-        elif resultat == "touché":
-            print("Vous avez touché un bateau !")
-        else:
-            print("Vous avez déjà touché cette case.")
+    if resultat_joueur == "raté":
+        print("Vous avez raté votre tir.")
+    elif resultat_joueur == "touché":
+        print("Vous avez touché un bateau !")
+    else:
+        print("Vous avez déjà touché cette case.")
+
     # Tour de l'ordinateur
     print("C'est au tour de l'ordinateur.")
-    x = random.randint(0, TAILLE_GRILLE - 1)
-    y = random.randint(0, TAILLE_GRILLE - 1)
-    resultat = tirer(grille_joueur, x, y)
-    print("L'ordinateur tire en ({}, {}).".format(x, y))
+    x_ordinateur = random.randint(0, TAILLE_GRILLE - 1)
+    y_ordinateur = random.randint(0, TAILLE_GRILLE - 1)
+    resultat_ordinateur = tirer(grille_joueur, x_ordinateur, y_ordinateur)
+    print("L'ordinateur tire en ({}, {}).".format(x_ordinateur, y_ordinateur))
     if all(SYMBOLS["bateau"] not in ligne for ligne in grille_joueur):
         print("L'ordinateur a gagné !")
         break
-    if resultat == "raté":
+    if resultat_ordinateur == "raté":
         print("L'ordinateur a raté son tir.")
-    elif resultat == "touché":
+    elif resultat_ordinateur == "touché":
         print("L'ordinateur a touché l'un de vos bateaux !")
     else:
         print("L'ordinateur a déjà touché cette case.")
-        afficher_grille(grille_joueur)
+    afficher_grille(grille_joueur)
